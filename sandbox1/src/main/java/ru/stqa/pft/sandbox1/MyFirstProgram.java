7268d3b8415513ab49583fba7a51836de270cb85
Julia <jula77@inbox.ru>
2019-08-07 22:20:19 +0300

    какие-то изменения))

--- sandbox1/src/main/java/ru/stqa/pft/sandbox1/MyFirstProgram.java
+++ sandbox1/src/main/java/ru/stqa/pft/sandbox1/MyFirstProgram.java
@@ -3,6 +3,11 @@ package ru.stqa.pft.sandbox1;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+      String somebody = "world";
+      System.out.println("Hello," + somebody + "!!!");
+
+      double l = 8.0;
+      double s = l * l;
+      System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 }
