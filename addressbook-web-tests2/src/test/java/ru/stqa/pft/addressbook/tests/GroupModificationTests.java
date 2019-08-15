610c3c35fff83b11531ce2fecdbf911a4b238a8a
Julia <jula77@inbox.ru>
2019-08-15 23:55:35 +0300

    Добавлен тест изменения группы
    При запуске этот тест почему-то доходит до нажатия на кнопку Edit group. То есть открывается список групп, отмечается галкой одна из них и все - на этом тест останавливается. Что не так в моем коде? Заранее спасибо)

---
+++ addressbook-web-tests2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test111", "test222", "test333"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+}
