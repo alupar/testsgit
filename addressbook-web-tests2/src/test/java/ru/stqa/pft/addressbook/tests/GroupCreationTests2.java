d27f2f262ba84a71d6292d1cd60e62d0e8497786
Julia <jula77@inbox.ru>
2019-08-24 20:55:15 +0300

    Добавлено определение количества групп в списке (пока не работает)

--- addressbook-web-tests2/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests2.java
+++ addressbook-web-tests2/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests2.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -8,8 +9,11 @@ public class GroupCreationTests2 extends TestBase {
 
   @Test
   public void testGroupCreation2() throws Exception {
+    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().createGroup(new GroupData("test111", null, null));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
   }
 
 }
