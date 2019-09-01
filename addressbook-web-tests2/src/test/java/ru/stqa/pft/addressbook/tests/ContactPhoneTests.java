edf352161ff480ed8ab5a89f78b3d26d22bf0aab
Julia <jula77@inbox.ru>
2019-09-02 00:39:23 +0300

    начало лекции 5.9

--- addressbook-web-tests2/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests2/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -3,10 +3,14 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-@Test
+
 public class ContactPhoneTests extends TestBase{
-  app.goTo().gotoHomePage();
-  app.contact().returnToHomePage();
-  ContactData contact = app.contact().all().iterator().next();
-  ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+  @Test
+  public void testContactPhones(){
+    app.contact().returnToHomePage();
+    ContactData contact = app.contact().all().iterator().next();
+    ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+  }
+
 }
