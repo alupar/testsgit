ebeed30fd330045bc743e12904cd99d9e3da042a
Julia <jula77@inbox.ru>
2019-09-22 16:56:01 +0300

    создан LoginTests

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -55,4 +55,11 @@ public class ApplicationManager {
     wd.quit();
   }
 
+  public HttpSession newSession(){
+    return new HttpSession(this);
+  }
+
+  public Object getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
