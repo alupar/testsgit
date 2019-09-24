4f0371842912c0de681fd9b9015863d8f05d8100
Julia <jula77@inbox.ru>
2019-09-25 00:22:51 +0300

    Реализована передача файлов на удалённую машину по протоколу FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -16,8 +16,10 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  public WebDriver wd;
+  private WebDriver wd;
   private String browser;
+  private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -27,17 +29,6 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    if (browser.equals(BrowserType.FIREFOX)){
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)){
-      wd = new InternetExplorerDriver();
-    }
-
-    wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
 
@@ -52,14 +43,45 @@ public class ApplicationManager {
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
   public HttpSession newSession(){
     return new HttpSession(this);
   }
 
-  public Object getProperty(String key) {
+  public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public FtpHelper ftp() {
+    if(ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null){
+      if (browser.equals(BrowserType.FIREFOX)){
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)){
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)){
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
