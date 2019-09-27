f00deabd295ae666f3ece57d5807a18938184ea1
Julia <jula77@inbox.ru>
2019-09-27 21:36:39 +0300

    закончила полную регистрацию пользователя с получением почты

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -35,7 +35,7 @@ public class HttpSession {
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpclient.execute(post);
     String body = getTextFrom(response);
-    System.out.println(body);
+//    System.out.println(body);
     return body.contains(String.format("<span class=\"user-info\">%s</span>", username));
   }
 
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -11,15 +11,15 @@ public class RegistrationHelper extends HelperBase {
   public void start(String username, String email) {
    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
    type(By.name("username"),username);
-    type(By.name("email"),email);
-    click(By.cssSelector("input[value='Зарегистрироваться']"));
+   type(By.name("email"),email);
+   click(By.cssSelector("input[value='Зарегистрироваться']"));
   }
 
   public void finish(String confirmationLink, String password, String username) {
     wd.get(confirmationLink);
-    type(By.name("username"),username);
+    type(By.name("realname"),username);
     type(By.name("password"), password);
     type(By.name("password_confirm"), password);
-    click(By.cssSelector("input[value='Зарегистрироваться']"));
+    click(By.cssSelector("button[type='submit']"));
   }
 }
