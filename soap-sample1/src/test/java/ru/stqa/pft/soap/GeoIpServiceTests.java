2c02a9406c6607c6bdb1bac06f50801543d350e9
Julia <jula77@inbox.ru>
2019-09-29 23:10:46 +0300

    RestTests: создание багрепортов

--- soap-sample1/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
+++ soap-sample1/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -3,9 +3,18 @@ package ru.stqa.pft.soap;
 import com.lavasoft.GeoIPService;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class GeoIpServiceTests {
   @Test
   public void testMyIp() {
     String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("194.28.29.152");
+    assertEquals(ipLocation.isEmpty(), false);
+  }
+
+  @Test
+  public void testInvalidIp() {
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("194.28.29.xxx");
+    assertEquals(ipLocation.endsWith("152"), true);
   }
 }
